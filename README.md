b0c31ab1e104b32640f644329ae928d70d1ff519
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2023-01-27 15:53:09 +0000

    issue#1 added dispatcher code

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # dispatcher-service
-Small service that streams reddit posts and comments to an endpoint
+
+Small service that streams reddit submissions and comments
+to respective endpoints
+
+## Reddit credentials
+
+Create new integration credentials at https://www.reddit.com/prefs/apps
+
+Set the credentials in `.env` file (DO NOT COMMIT IT PLEASE).
+```env
+REDDIT_CLIENT_ID=<value>
+REDDIT_CLIENT_SECRET=<value>
+```
+
+## Local dev / docker
+
+Build and run using the environment variables in `.env` file
+and `config.json`:
+
+```sh
+docker build -t dispatcher . && docker run -it --env-file .env -v $(pwd)/config.json:/src/config.json dispatcher
+```
+
+>Note: if you're running the code outside the docker container,
+>you need to set `CONFIG_PATH` environment variable to your `config.json` path.
---
+++ app/dispatcher.py
@@ -0,0 +1,92 @@
+import sys
+import json
+import socket
+import logging
+import requests
+import time
+import asyncpraw
+import asyncio
+
+from asyncpraw.models.reddit.submission import Submission
+from asyncpraw.models.reddit.comment import Comment
+
+
+class RedditDispatcher:
+    """
+    Dispatcher class that streams data from Reddit API and
+    sends it to the consumer endpoints.
+    """
+
+    def __init__(self, **kwargs):
+        self._setup_logging(kwargs.get("debug", False))
+        api_config = kwargs.get("api_config", dict())
+
+        self.submission_endpoint = kwargs.get("submission_endpoint", "http://localhost:8080")
+        self.logger.info(f"Set submission endpoint to {self.submission_endpoint}")
+
+        self.comment_endpoint = kwargs.get("comment_endpoint", "http://localhost:8080")
+        self.logger.info(f"Set comment endpoint to {self.comment_endpoint}")
+
+        self.subreddits = kwargs.get("subreddits", [])
+        self.logger.info(f"Watching subreddits {self.subreddits}")
+
+        self.reddit = asyncpraw.Reddit(**api_config)
+        self.headers = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+    def _setup_logging(self, debug=False):
+        """Set up the logger object
+        """
+        self.logger = logging.getLogger("dispatcher")
+        self.logger.setLevel(logging.DEBUG if debug else logging.INFO)
+        self.logger.debug("Running with debug ON")
+
+    def start(self) -> None:
+        """Starts streaming reddit activity and sending it to the bots.
+        """
+        loop = asyncio.get_event_loop()
+        loop.create_task(self._stream_comments())
+        loop.create_task(self._stream_submissions())
+        self.logger.info("Started reddit dispatcher")
+        loop.run_forever()
+
+    async def _stream_submissions(self) -> None:
+        """Stream submissions asynchronously. Calls self._dispatch_submission()
+        """
+        subreddit = await self.reddit.subreddit(
+            "+".join(self.subreddits))
+        async for submission in subreddit.stream.submissions(pause_after=-1):
+            if submission is None:
+                continue
+            await self._dispatch(self.submission_endpoint, dict(
+                id=submission.id,
+                created_utc=submission.created_utc
+            ))
+
+    async def _stream_comments(self) -> None:
+        """Stream comments asynchronously. Calls self._dispatch_comment()
+        """
+        subreddit = await self.reddit.subreddit(
+            "+".join(self.subreddits))
+        async for comment in subreddit.stream.comments(pause_after=-1):
+            if comment is None:
+                continue
+            await self._dispatch(self.comment_endpoint, dict(
+                id=comment.id,
+                created_utc=comment.created_utc
+            ))
+
+    async def _dispatch(self, endpoint: str, data: dict) -> None:
+        """POST a json data object to an endpoint
+        """
+        try:
+            response = requests.post(endpoint,
+                data=json.dumps(data),
+                headers=self.headers
+            ).raise_for_status()
+            self.logger.debug(f"Dispatched comment with ID={submission.id}")
+        except Exception as e:
+            self.logger.error(f"Failed to dispatch to {endpoint}, {e}")
+            time.sleep(1) # wait for a bit to not flood the logs
